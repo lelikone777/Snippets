9e4f2fa976bd4dd5cfdb77fa734bd5091ceb233c
KALININ ALEXEY <2257855@gmail.com>
2021-02-06 16:18:12 +0300

    1

---
+++ 1/script.js
@@ -0,0 +1,23 @@
+
+const menu = document.querySelector('.menu');
+const button = document.querySelector('.button');
+
+
+
+button.addEventListener('click', event => {
+    const target = event.target;
+    
+   
+      if (!button.classList.contains('active')) { 
+        button.classList.add('active');
+        menu.classList.add('open');
+      } else {
+        button.classList.remove('active');
+        menu.classList.add('close');
+        setTimeout(() => {
+          menu.classList.remove('open');
+          menu.classList.remove('close');
+        }, 800)
+      }
+    
+  });
